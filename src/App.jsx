c915d0a68bebe019c1ee1ab7c455aac59f182773
Mrcoder <amirsohail4151@gmail.com>
2024-06-13 10:31:38 -0700

    Hover Card Effect

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+
+  return (
+
+    <div class="card">
+
+      <p> <span>  SUBCRIBE  </span> </p>
+
+      <p> <span>  MR CODER  </span> </p>
+
+      <p> <span>  CHANNEL   </span> </p>
+
+    </div>
+  )
+}
+
+export default App
